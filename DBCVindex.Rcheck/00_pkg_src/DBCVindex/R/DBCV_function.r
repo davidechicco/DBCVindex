6fb13ff98f941f655ba202e7be8bd90077154da0
davidechicco <davide.chicco@gmail.com>
2025-02-12 16:06:27 +0100

    edited

--- DBCVindex.Rcheck/00_pkg_src/DBCVindex/R/DBCV_function.r
+++ DBCVindex.Rcheck/00_pkg_src/DBCVindex/R/DBCV_function.r
@@ -1,16 +1,149 @@
+
+#' Function that calculates the mutual reachability distance within a matrix
+#'
+#' @param MinPts number of minimal points
+#' @param G_edges_weights matrix of edges weights
+#' @param d number of features
+#' @export
+#' @import qpdf
+#' @return a list of two elements: d_ucore and G_edges_weights:
+#' @examples
+#'
+#'  n = 300; noise = 0.05; seed = 1782;
+#'  theta <- seq(0, pi, length.out = n / 2)
+#'  x1 <- cos(theta) + rnorm(n / 2, sd = noise)
+#'  y1 <- sin(theta) + rnorm(n / 2, sd = noise)
+#'  x2 <- cos(theta + pi) + rnorm(n / 2, sd = noise)
+#'  y2 <- sin(theta + pi) + rnorm(n / 2, sd = noise)
+#'  X <- rbind(cbind(x1, y1), cbind(x2, y2))
+#'  y <- c(rep(0, n / 2), rep(1, n / 2))
+#'
+#' nfeatures <- ncol(X)
+#' i <- 1
+#' clusters <- unique(y)
+#' objcl <- which(y == clusters[i])
+#' nuobjcl <- length(objcl)
+#'
+#' noiseLabel <- -1
+#' distX <- as.matrix(dist(X))^2
+#' distXy <- distX[y != noiseLabel, y != noiseLabel]
+#'
+#' mr <- matrix_mutual_reachability_distance(nuobjcl, distXy[objcl, objcl], nfeatures)
+matrix_mutual_reachability_distance <- function(MinPts, G_edges_weights, d) {
+  No <- nrow(G_edges_weights)
+
+  K_NN_Dist <- G_edges_weights^(-1 * d)
+  K_NN_Dist[K_NN_Dist == Inf] <- 0
+
+  d_ucore <- colSums(K_NN_Dist)
+  d_ucore <- d_ucore / (No - 1)
+  d_ucore <- (1 / d_ucore)^(1 / (1 * d))
+  d_ucore[d_ucore == Inf] <- 0
+
+  for (i in 1:No) {
+    for (j in 1:MinPts) {
+      G_edges_weights[i, j] <- max(c(d_ucore[i], d_ucore[j], G_edges_weights[i, j]))
+      G_edges_weights[j, i] <- G_edges_weights[i, j]
+    }
+  }
+
+  return(list(d_ucore = d_ucore, G_edges_weights = G_edges_weights))
+}
+
+#' Function that finds the list of MST edges
+#'
+#' @param G list of four elements: number of vertices, MST_edges (matrix of edges),
+#'                  MST_degrees (array of numbers), MST_parent (array of numbers)
+#' @param start index of the first edge
+#' @param G_edges_weights matrix of edges weights
+#' @export
+#' @import qpdf
+#' @return list of two elements: matrix of edges and array of degrees
+#' @examples
+#' n = 300; noise = 0.05;
+#' seed = 1782;
+#' theta <- seq(0, pi, length.out = n / 2)
+#' x1 <- cos(theta) + rnorm(n / 2, sd = noise)
+#' y1 <- sin(theta) + rnorm(n / 2, sd = noise)
+#' x2 <- cos(theta + pi) + rnorm(n / 2, sd = noise)
+#' y2 <- sin(theta + pi) + rnorm(n / 2, sd = noise)
+#' X <- rbind(cbind(x1, y1), cbind(x2, y2))
+#'  y <- c(rep(0, n / 2), rep(1, n / 2))
+#'
+#' nfeatures <- ncol(X)
+#' i <- 1
+#' clusters <- unique(y)
+#' objcl <- which(y == clusters[i])
+#' nuobjcl <- length(objcl)
+#'
+#' noiseLabel <- -1
+#' distX <- as.matrix(dist(X))^2
+#' distXy <- distX[y != noiseLabel, y != noiseLabel]
+#'
+#' mr <- matrix_mutual_reachability_distance(nuobjcl, distXy[objcl, objcl], nfeatures)
+#'
+#' d_ucore_cl <- rep(0, nrow(X))
+#' d_ucore_cl[objcl] <- mr$d_ucore
+#' G <- list(no_vertices = nuobjcl, MST_edges = matrix(0, nrow = nuobjcl - 1, ncol = 3),
+#'          MST_degrees = rep(0, nuobjcl), MST_parent = rep(0, nuobjcl))
+#' g_start <- 1
+#'
+#' mst_results <- MST_Edges(G, g_start, mr$G_edges_weights)
+#'
+MST_Edges <- function(G, start, G_edges_weights) {
+  intree <- rep(0, G$no_vertices)
+  d <- rep(Inf, G$no_vertices)
+  G$MST_parent <- 1:G$no_vertices
+
+  d[start] <- 0
+  v <- start
+  counter <- 0
+
+  while (counter < (G$no_vertices - 1)) {
+    intree[v] <- 1
+    this_dist <- Inf
+
+    for (w in 1:G$no_vertices) {
+      if (w != v && intree[w] == 0) {
+        weight <- G_edges_weights[v, w]
+        if (d[w] > weight) {
+          d[w] <- weight
+          G$MST_parent[w] <- v
+        }
+        if (this_dist > d[w]) {
+          this_dist <- d[w]
+          next_v <- w
+        }
+      }
+    }
+
+    counter <- counter + 1
+    G$MST_edges[counter, ] <- c(G$MST_parent[next_v], next_v, G_edges_weights[G$MST_parent[next_v], next_v])
+    G$MST_degrees[G$MST_parent[next_v]] <- G$MST_degrees[G$MST_parent[next_v]] + 1
+    G$MST_degrees[next_v] <- G$MST_degrees[next_v] + 1
+    v <- next_v
+  }
+
+  Edg <- G$MST_edges
+  Degr <- G$MST_degrees
+
+  return(list(Edg = Edg, Degr = Degr))
+}
+
+
 #' Function that calculates the Density-Based Clustering Validation index (DBCV) of clustering results
 #'
 #' @param data input clustering results
 #' @param partition labels of the clustering
 #' @param noiseLabel the code of the noise cluster points, -1 by default
 #' @export
-#' @import ape vegan emstreeR devtools
+#' @import qpdf
 #' @return a real value containing the DBCV coefficient in the [-1;+1] interval
 #' @examples
 #'
-#' n = 300; noise = 0.05;
+#'  n = 300; noise = 0.05;
 #'  seed = 1782;
-#' theta <- seq(0, pi, length.out = n / 2)
+#'  theta <- seq(0, pi, length.out = n / 2)
 #'  x1 <- cos(theta) + rnorm(n / 2, sd = noise)
 #'  y1 <- sin(theta) + rnorm(n / 2, sd = noise)
 #'  x2 <- cos(theta + pi) + rnorm(n / 2, sd = noise)
@@ -22,12 +155,12 @@
 #'
 dbcv <- function(data, partition, noiseLabel = -1) {
   clusters <- unique(partition)
-  dist <- as.matrix(dist(data))^2
+  distX <- as.matrix(dist(data))^2
 
   for (i in seq_along(clusters)) {
     if (sum(partition == clusters[i]) == 1) {
       partition[partition == clusters[i]] <- noiseLabel
-      clusters[i] <- noiseLabels
+      clusters[i] <- noiseLabel
     }
   }
 
@@ -38,7 +171,7 @@ dbcv <- function(data, partition, noiseLabel = -1) {
   }
 
   data <- data[partition != noiseLabel, ]
-  dist <- dist[partition != noiseLabel, partition != noiseLabel]
+  distXy <- distX[partition != noiseLabel, partition != noiseLabel]
   poriginal <- partition
   partition <- partition[partition != noiseLabel]
 
@@ -55,7 +188,7 @@ dbcv <- function(data, partition, noiseLabel = -1) {
     objcl <- which(partition == clusters[i])
     nuobjcl <- length(objcl)
 
-    mr <- matrix_mutual_reachability_distance(nuobjcl, dist[objcl, objcl], nfeatures)
+    mr <- matrix_mutual_reachability_distance(nuobjcl, distXy[objcl, objcl], nfeatures)
     d_ucore_cl[objcl] <- mr$d_ucore
     G <- list(no_vertices = nuobjcl, MST_edges = matrix(0, nrow = nuobjcl - 1, ncol = 3),
               MST_degrees = rep(0, nuobjcl), MST_parent = rep(0, nuobjcl))
@@ -84,7 +217,7 @@ dbcv <- function(data, partition, noiseLabel = -1) {
   sep_point <- matrix(0, nrow = nobjects, ncol = nobjects)
   for (i in 1:(nobjects - 1)) {
     for (j in i:nobjects) {
-      sep_point[i, j] <- max(c(dist[i, j], d_ucore_cl[i], d_ucore_cl[j]))
+      sep_point[i, j] <- max(c(distXy[i, j], d_ucore_cl[i], d_ucore_cl[j]))
       sep_point[j, i] <- sep_point[i, j]
     }
   }
@@ -103,121 +236,5 @@ dbcv <- function(data, partition, noiseLabel = -1) {
 
   valid <- valid / length(poriginal)
   return(valid)
-}
-
-#' Function that calculates the mutual reachability distance within a matrix
-#'
-#' @param MinPts number of minimal points
-#' @param G_edges_weights matrix of edges weights
-#' @param d number of features
-#' @export
-#' @import ape vegan emstreeR devtools
-#' @return a list of two elements: d_ucore and G_edges_weights:
-#' @examples
-#'
-#' n = 300; noise = 0.05; seed = 1782;
-#' theta <- seq(0, pi, length.out = n / 2)
-#'  x1 <- cos(theta) + rnorm(n / 2, sd = noise)
-#'  y1 <- sin(theta) + rnorm(n / 2, sd = noise)
-#'  x2 <- cos(theta + pi) + rnorm(n / 2, sd = noise)
-#'  y2 <- sin(theta + pi) + rnorm(n / 2, sd = noise)
-#'  X <- rbind(cbind(x1, y1), cbind(x2, y2))
-#'
-#' nfeatures <- ncol(X)
-#' i <- 1
-#' objcl <- which(partition == clusters[i])
-#' nuobjcl <- length(objcl)
-#'
-#' mr <- matrix_mutual_reachability_distance(nuobjcl, dist[objcl, objcl], nfeatures)
-matrix_mutual_reachability_distance <- function(MinPts, G_edges_weights, d) {
-  No <- nrow(G_edges_weights)
-
-  K_NN_Dist <- G_edges_weights^(-1 * d)
-  K_NN_Dist[K_NN_Dist == Inf] <- 0
-
-  d_ucore <- colSums(K_NN_Dist)
-  d_ucore <- d_ucore / (No - 1)
-  d_ucore <- (1 / d_ucore)^(1 / (1 * d))
-  d_ucore[d_ucore == Inf] <- 0
-
-  for (i in 1:No) {
-    for (j in 1:MinPts) {
-      G_edges_weights[i, j] <- max(c(d_ucore[i], d_ucore[j], G_edges_weights[i, j]))
-      G_edges_weights[j, i] <- G_edges_weights[i, j]
-    }
-  }
-
-  return(list(d_ucore = d_ucore, G_edges_weights = G_edges_weights))
-}
-
-#' Function that finds the list of MST edges
-#'
-#' @param G list of four elements: number of vertices, MST_edges (matrix of edges),  MST_degrees (array of numbers), MST_parent (array of numbers)
-#' @param start index of the first edge
-#' @param G_edges_weights matrix of edges weights
-#' @export
-#' @import ape vegan emstreeR devtools
-#' @return list of two elements: matrix of edges and array of degrees
-#' @examples
-#' n = 300; noise = 0.05;
-#' seed = 1782;
-#' theta <- seq(0, pi, length.out = n / 2)
-#' x1 <- cos(theta) + rnorm(n / 2, sd = noise)
-#' y1 <- sin(theta) + rnorm(n / 2, sd = noise)
-#' x2 <- cos(theta + pi) + rnorm(n / 2, sd = noise)
-#' y2 <- sin(theta + pi) + rnorm(n / 2, sd = noise)
-#' X <- rbind(cbind(x1, y1), cbind(x2, y2))
-#'
-#' nfeatures <- ncol(X)
-#' i <- 1
-#' objcl <- which(partition == clusters[i])
-#' nuobjcl <- length(objcl)
-#'
-#' mr <- matrix_mutual_reachability_distance(nuobjcl, dist[objcl, objcl], nfeatures)
-#'
-#' d_ucore_cl[objcl] <- mr$d_ucore
-#' G <- list(no_vertices = nuobjcl, MST_edges = matrix(0, nrow = nuobjcl - 1, ncol = 3), MST_degrees = rep(0, nuobjcl), MST_parent = rep(0, nuobjcl))
-#' g_start <- 1
-#'
-#' mst_results <- MST_Edges(G, g_start, mr$G_edges_weights)
-#'
-MST_Edges <- function(G, start, G_edges_weights) {
-  intree <- rep(0, G$no_vertices)
-  d <- rep(Inf, G$no_vertices)
-  G$MST_parent <- 1:G$no_vertices
-
-  d[start] <- 0
-  v <- start
-  counter <- 0
-
-  while (counter < (G$no_vertices - 1)) {
-    intree[v] <- 1
-    dist <- Inf
-
-    for (w in 1:G$no_vertices) {
-      if (w != v && intree[w] == 0) {
-        weight <- G_edges_weights[v, w]
-        if (d[w] > weight) {
-          d[w] <- weight
-          G$MST_parent[w] <- v
-        }
-        if (dist > d[w]) {
-          dist <- d[w]
-          next_v <- w
-        }
-      }
-    }
-
-    counter <- counter + 1
-    G$MST_edges[counter, ] <- c(G$MST_parent[next_v], next_v, G_edges_weights[G$MST_parent[next_v], next_v])
-    G$MST_degrees[G$MST_parent[next_v]] <- G$MST_degrees[G$MST_parent[next_v]] + 1
-    G$MST_degrees[next_v] <- G$MST_degrees[next_v] + 1
-    v <- next_v
-  }
-
-  Edg <- G$MST_edges
-  Degr <- G$MST_degrees
-
-  return(list(Edg = Edg, Degr = Degr))
 }
 
---
+++ DBCVindex.Rcheck/DBCVindex-Ex.R
@@ -0,0 +1,151 @@
+pkgname <- "DBCVindex"
+source(file.path(R.home("share"), "R", "examples-header.R"))
+options(warn = 1)
+base::assign(".ExTimings", "DBCVindex-Ex.timings", pos = 'CheckExEnv')
+base::cat("name\tuser\tsystem\telapsed\n", file=base::get(".ExTimings", pos = 'CheckExEnv'))
+base::assign(".format_ptime",
+function(x) {
+  if(!is.na(x[4L])) x[1L] <- x[1L] + x[4L]
+  if(!is.na(x[5L])) x[2L] <- x[2L] + x[5L]
+  options(OutDec = '.')
+  format(x[1L:3L], digits = 7L)
+},
+pos = 'CheckExEnv')
+
+### * </HEADER>
+library('DBCVindex')
+
+base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
+base::assign(".old_wd", base::getwd(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("MST_Edges")
+### * MST_Edges
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: MST_Edges
+### Title: Function that finds the list of MST edges
+### Aliases: MST_Edges
+
+### ** Examples
+
+n = 300; noise = 0.05;
+seed = 1782;
+theta <- seq(0, pi, length.out = n / 2)
+x1 <- cos(theta) + rnorm(n / 2, sd = noise)
+y1 <- sin(theta) + rnorm(n / 2, sd = noise)
+x2 <- cos(theta + pi) + rnorm(n / 2, sd = noise)
+y2 <- sin(theta + pi) + rnorm(n / 2, sd = noise)
+X <- rbind(cbind(x1, y1), cbind(x2, y2))
+ y <- c(rep(0, n / 2), rep(1, n / 2))
+
+nfeatures <- ncol(X)
+i <- 1
+clusters <- unique(y)
+objcl <- which(y == clusters[i])
+nuobjcl <- length(objcl)
+
+noiseLabel <- -1
+distX <- as.matrix(dist(X))^2
+distXy <- distX[y != noiseLabel, y != noiseLabel]
+
+mr <- matrix_mutual_reachability_distance(nuobjcl, distXy[objcl, objcl], nfeatures)
+
+d_ucore_cl <- rep(0, nrow(X))
+d_ucore_cl[objcl] <- mr$d_ucore
+G <- list(no_vertices = nuobjcl, MST_edges = matrix(0, nrow = nuobjcl - 1, ncol = 3),
+         MST_degrees = rep(0, nuobjcl), MST_parent = rep(0, nuobjcl))
+g_start <- 1
+
+mst_results <- MST_Edges(G, g_start, mr$G_edges_weights)
+
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("MST_Edges", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("dbcv")
+### * dbcv
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: dbcv
+### Title: Function that calculates the Density-Based Clustering Validation
+###   index (DBCV) of clustering results
+### Aliases: dbcv
+
+### ** Examples
+
+
+ n = 300; noise = 0.05;
+ seed = 1782;
+ theta <- seq(0, pi, length.out = n / 2)
+ x1 <- cos(theta) + rnorm(n / 2, sd = noise)
+ y1 <- sin(theta) + rnorm(n / 2, sd = noise)
+ x2 <- cos(theta + pi) + rnorm(n / 2, sd = noise)
+ y2 <- sin(theta + pi) + rnorm(n / 2, sd = noise)
+ X <- rbind(cbind(x1, y1), cbind(x2, y2))
+ y <- c(rep(0, n / 2), rep(1, n / 2))
+
+cat("dbcv(X, y) = ", dbcv(X, y), "\n", sep="")
+
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("dbcv", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("matrix_mutual_reachability_distance")
+### * matrix_mutual_reachability_distance
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: matrix_mutual_reachability_distance
+### Title: Function that calculates the mutual reachability distance within
+###   a matrix
+### Aliases: matrix_mutual_reachability_distance
+
+### ** Examples
+
+
+ n = 300; noise = 0.05; seed = 1782;
+ theta <- seq(0, pi, length.out = n / 2)
+ x1 <- cos(theta) + rnorm(n / 2, sd = noise)
+ y1 <- sin(theta) + rnorm(n / 2, sd = noise)
+ x2 <- cos(theta + pi) + rnorm(n / 2, sd = noise)
+ y2 <- sin(theta + pi) + rnorm(n / 2, sd = noise)
+ X <- rbind(cbind(x1, y1), cbind(x2, y2))
+ y <- c(rep(0, n / 2), rep(1, n / 2))
+
+nfeatures <- ncol(X)
+i <- 1
+clusters <- unique(y)
+objcl <- which(y == clusters[i])
+nuobjcl <- length(objcl)
+
+noiseLabel <- -1
+distX <- as.matrix(dist(X))^2
+distXy <- distX[y != noiseLabel, y != noiseLabel]
+
+mr <- matrix_mutual_reachability_distance(nuobjcl, distXy[objcl, objcl], nfeatures)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("matrix_mutual_reachability_distance", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+### * <FOOTER>
+###
+cleanEx()
+options(digits = 7L)
+base::cat("Time elapsed: ", proc.time() - base::get("ptime", pos = 'CheckExEnv'),"\n")
+grDevices::dev.off()
+###
+### Local variables: ***
+### mode: outline-minor ***
+### outline-regexp: "\\(> \\)?### [*]+" ***
+### End: ***
+quit('no')
